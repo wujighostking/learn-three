9dce4d354e3fa8c5578b1b70731d514ebb0e6be9
无极鬼王 <1469285270@qq.com>
2023-02-12 23:22:14 +0800

    添加css3dSprites（未完成）

--- src/09.css3dSprite.ts
+++ src/09.css3dSprite.ts
@@ -1,13 +1,21 @@
 import { getUrl } from './utils/path'
 import {
-  CSS3DObject,
   CSS3DRenderer,
+  CSS3DSprite,
 } from 'three/examples/jsm/renderers/CSS3DRenderer'
+import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls'
+import { TWEEN } from 'three/examples/jsm/libs/tween.module.min'
 import { PerspectiveCamera, Scene } from 'three'
 
+const count: number = 125
+const cubic = Math.pow(count, 1 / 3)
 let css3dRenderer: CSS3DRenderer
 let scene: Scene
 let perspectiveCamera: PerspectiveCamera
+let controls: TrackballControls
+let domRenderer: HTMLElement
+let objectList: CSS3DSprite[] = []
+let isCube: boolean = false
 
 function init() {
   scene = new Scene()
@@ -18,38 +26,81 @@ function init() {
     0.01,
     100000
   )
-}
-
-function createCss3dObject() {
   perspectiveCamera.position.set(600, 400, 1500)
   perspectiveCamera.lookAt(0, 0, 0)
   scene.add(perspectiveCamera)
-  for (let i = 0; i < 100; i++) {
+}
+
+function createCss3dObject() {
+  for (let i = 0; i < count; i++) {
     const img = document.createElement('img')
     img.src = getUrl('models/texture/sprite.png')
+    img.addEventListener('click', () => {
+      console.log('i: ', img.style.height)
+    })
 
-    const object = new CSS3DObject(img)
-    object.position.x = Math.random() * 4000 - 2000
-    object.position.y = Math.random() * 4000 - 2000
-    object.position.z = Math.random() * 4000 - 2000
+    const object = new CSS3DSprite(img)
+    object.position.x = Math.random() * 2000 - 1000
+    object.position.y = Math.random() * 2000 - 1000
+    object.position.z = Math.random() * 2000 - 1000
+    objectList.push(object)
     scene.add(object)
   }
 }
 
 function createCss3dRender() {
   css3dRenderer = new CSS3DRenderer()
+  domRenderer = css3dRenderer.domElement
   css3dRenderer.setSize(window.innerWidth, window.innerHeight)
   document.body.appendChild(css3dRenderer.domElement)
+
+  window.addEventListener('resize', () => {
+    const innerWidth = window.innerWidth
+    const innerHeight = window.innerHeight
+    perspectiveCamera.aspect = innerWidth / innerHeight
+    perspectiveCamera.updateProjectionMatrix()
+    css3dRenderer.setSize(innerWidth, innerHeight)
+  })
+}
+
+function initControls() {
+  controls = new TrackballControls(perspectiveCamera, domRenderer)
 }
 
 function animate() {
-  requestAnimationFrame(animate)
   css3dRenderer.render(scene, perspectiveCamera)
+  controls.update()
+  requestAnimationFrame(animate)
+}
+
+function createPosition() {
+  if (!isCube) {
+    for (let x = 0; x < cubic; x++) {
+      for (let y = 0; y < cubic; y++) {
+        for (let z = 0; z < cubic; z++) {
+          const index = Math.ceil(z + y * cubic + x * cubic ** 2)
+          objectList[index].position.set(x * 100, y * 100, z * 100)
+        }
+      }
+    }
+  } else {
+    objectList.forEach((object) => {
+      object.position.x = Math.random() * 2000 - 1000
+      object.position.y = Math.random() * 2000 - 1000
+      object.position.z = Math.random() * 2000 - 1000
+    })
+  }
+  isCube = !isCube
 }
 
 init()
 createCss3dObject()
 createCss3dRender()
+initControls()
 animate()
+
+setInterval(() => {
+  createPosition()
+}, 3000)
 
 export {}
